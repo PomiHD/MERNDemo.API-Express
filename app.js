e958f687c935bb439bb7e82b7f2e9516229c9639
Samuel Yin (Ciallo~) <wsgddjy@live.com>
2024-05-27 19:18:45 +1000

    90. Implementing Basic Routing

---
+++ app.js
@@ -0,0 +1,10 @@
+﻿const express = require("express");
+const bodyParser = require("body-parser");
+
+const placeRoutes = require("./routes/places-route");
+
+const app = express();
+
+app.use(placeRoutes);
+
+app.listen(5000);
---
+++ routes/places-route.js
@@ -0,0 +1,10 @@
+﻿const express = require("express");
+
+const router = express.Router();
+
+router.get("/", (req, res, next) => {
+  console.log("GET request in places route");
+  res.json({ message: "It works!" });
+});
+
+module.exports = router;
