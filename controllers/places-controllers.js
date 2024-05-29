1f4761fb170b6976d7c81cacc59e3bf4d86fd48d
Samuel Yin (Ciallo~) <wsgddjy@live.com>
2024-05-29 13:31:47 +1000

    95. Adding Controllers

---
+++ controllers/places-controllers.js
@@ -0,0 +1,45 @@
+﻿const HttpError = require("../models/http-error");
+
+const DUMMY_PLACES = [
+  {
+    id: "p1",
+    title: "Empire State Building",
+    description: "One of the most famous sky scrapers in the world!",
+    imageUrl:
+      "https://upload.wikimedia.org/wikipedia/commons/1/10/Empire_State_Building_%28aerial_view%29.jpg",
+    address: "20 W 34th St, New York, NY 10118",
+    location: {
+      lat: 40.7484405,
+      lng: -73.9878531,
+    },
+    creator: "u1",
+  },
+];
+
+const getPlaceById = (req, res, next) => {
+  const placeId = req.params.pid;
+  const place = DUMMY_PLACES.find((place) => place.id === placeId);
+  if (!place) {
+    // different way to throw an error, this is for sync code
+    throw new HttpError("Could not find a place for the provided id.", 404); // This will be caught in the error handling middleware
+  }
+  res.json({ place });
+};
+
+const getPlacesByUserId = (req, res, next) => {
+  const userId = req.params.uid;
+  const place = DUMMY_PLACES.find((place) => place.creator === userId);
+  if (!place) {
+    // different way to throw an error, this is for async code
+    return next(
+      new HttpError("Could not find a place for the provided user id.", 404),
+    ); // This will be caught in the error handling middleware
+  }
+  res.json({ place });
+};
+// alternative ways
+// function getPlaceById() {...}
+// const getPlaceById = function() {...}
+
+exports.getPlaceById = getPlaceById;
+exports.getPlacesByUserId = getPlacesByUserId;
--- routes/places-routes.js
+++ routes/places-routes.js
@@ -1,43 +1,22 @@
 ﻿const express = require("express");
 
-const HttpError = require("../models/http-error");
+const {
+  getPlaceById,
+  getPlacesByUserId,
+} = require("../controllers/places-controllers");
+
 const router = express.Router();
 
-const DUMMY_PLACES = [
-  {
-    id: "p1",
-    title: "Empire State Building",
-    description: "One of the most famous sky scrapers in the world!",
-    imageUrl:
-      "https://upload.wikimedia.org/wikipedia/commons/1/10/Empire_State_Building_%28aerial_view%29.jpg",
-    address: "20 W 34th St, New York, NY 10118",
-    location: {
-      lat: 40.7484405,
-      lng: -73.9878531,
-    },
-    creator: "u1",
-  },
-];
-router.get("/:pid", (req, res, next) => {
-  const placeId = req.params.pid;
-  const place = DUMMY_PLACES.find((place) => place.id === placeId);
-  if (!place) {
-    // different way to throw an error, this is for sync code
-    throw new HttpError("Could not find a place for the provided id.", 404); // This will be caught in the error handling middleware
-  }
-  res.json({ place });
-});
+/**
+ * @route   GET api/places/:pid
+ * @desc    Get place by id
+ */
+router.get("/:pid", getPlaceById);
 
-router.get("/user/:uid", (req, res, next) => {
-  const userId = req.params.uid;
-  const place = DUMMY_PLACES.find((place) => place.creator === userId);
-  if (!place) {
-    // different way to throw an error, this is for async code
-    return next(
-      new HttpError("Could not find a place for the provided user id.", 404),
-    ); // This will be caught in the error handling middleware
-  }
-  res.json({ place });
-});
+/**
+ * @route   GET api/places/user/:uid
+ * @desc    Get place by user id
+ */
+router.get("/user/:uid", getPlacesByUserId);
 
 module.exports = router;
