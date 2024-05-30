9b9a350c69ea07e1369ca7e3a66db2765bab28c9
Samuel Yin (Ciallo~) <wsgddjy@live.com>
2024-05-31 01:21:25 +1000

    99. Deleting Places

--- controllers/places-controllers.js
+++ controllers/places-controllers.js
@@ -1,7 +1,7 @@
 ﻿const HttpError = require("../models/http-error");
 const { v4: uuidv4 } = require("uuid");
 
-const DUMMY_PLACES = [
+let DUMMY_PLACES = [
   {
     id: "p1",
     title: "Empire State Building",
@@ -73,7 +73,11 @@ const updatePlace = (req, res, next) => {
   }
 };
 
-const deletePlace = (req, res, next) => {};
+const deletePlace = (req, res, next) => {
+  const placeId = req.params.pid;
+  DUMMY_PLACES = DUMMY_PLACES.filter((place) => place.id !== placeId);
+  res.status(200).json({ message: "Deleted place." });
+};
 
 exports.getPlaceById = getPlaceById;
 exports.getPlacesByUserId = getPlacesByUserId;
